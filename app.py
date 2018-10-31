71a18fa81689c741027f021a4189fc7f3845224e
Young Guo <young.guo@gmail.com>
2018-10-31 16:34:21 -0700

    Update README and clean app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
+from flask import Flask
 import os
-from flask import Flask, render_template, g, request
-import requests
-import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def index(name=None):
-    return 'hello world';
+def index():
+    return 'Hello, World!';
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port, debug=True)
     app.run(debug=True)
