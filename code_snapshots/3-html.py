1574f94a8f30fafbe28761fde9561914e9dc3dcc
Amitav Baruah <abaruah@berkeley.edu>
2018-11-01 15:05:44 -0700

    Update snapshots to reflect routing updates

--- code_snapshots/3-html.py
+++ code_snapshots/3-html.py
@@ -18,20 +18,18 @@ def category():
 	category_json = categories.json()
 	return categories.text
 
-@app.route('/category/<category_id>')
-def category_question(category_id = None):
-    questions = requests.get('http://jservice.io/api/category/', {
-        'id': category_id
-    }).json()['clues']
-	question_obj = random.choice(questions)
-	question, answer = question_obj['question'], question_obj['answer']
-	return question + ' ' + answer
 
 @app.route('/question')
-def question():
-    question_obj = requests.get('http://jservice.io/api/random').json()[0]
-	question, answer = question_obj['question'], question_obj['answer']
-	return question + ' ' + answer
+@app.route('/question/<category_id>')
+def get_random_question(category_id=None):
+    if category_id:
+        question_list = requests.get('http://jservice.io/api/category', data={'id': category_id}).json()['clues']
+        question_obj = random.choice(question_list)
+    else:
+        question_obj = requests.get('http://jservice.io/api/random').json()[0]
+    question, answer = question_obj['question'], question_obj['answer']
+    return question + ', ' + answer
+
 
 @app.route('/answer')
 def check_answer():
