bde7cd2c692c703d7e91c22f400168ea5da3d871
Amitav Baruah <abaruah@berkeley.edu>
2018-11-01 17:41:24 -0700

    Update snapshots

--- code_snapshots/1-flask.py
+++ code_snapshots/1-flask.py
@@ -17,12 +17,6 @@ def category():
 @app.route('/question')
 @app.route('/question/<category_id>')
 def question(category_id=None):
-    if category_id:
-        question_list = requests.get('http://jservice.io/api/category', params = {'id': category_id}).json()['clues']
-        question_obj = random.choice(question_list)
-    else:
-        question_obj = requests.get('http://jservice.io/api/random').json()[0]
-    question, answer = question_obj['question'], question_obj['answer']
     return question + ',' + answer
 
 @app.route('/answer')
